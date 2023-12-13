924ec74c5a7c8abfcf879c2fd8c483e288ebf8af
rajkonkret <rajkonkret660@gmail.com>
2023-12-13 14:28:38 +0100

    1 - add class for Currency and Cantor offer

--- app.py
+++ app.py
@@ -3,6 +3,34 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
+class Currency:
+
+    def __init__(self, code, name, flag):
+        self.code = code
+        self.name = name
+        self.flag = flag
+
+    def __repr__(self):
+        return f'<Currency {self.code}'
+
+
+class CantorOffer:
+    def __init__(self):
+        self.currencies = []
+
+    def load_offer(self):
+        self.currencies.append(Currency('USD', 'Dollar', 'flag_usa.png'))
+        self.currencies.append(Currency('EUR', 'Euro', 'flag_euro.png'))
+        self.currencies.append(Currency('JPY', 'Yen', 'flag_japan.png'))
+
+    def get_by_code(self, code):
+        for currency in self.currencies:
+            if currency.code == code:
+                return currency
+        return Currency('unknown', 'unknown', 'flag_pirat.png')
+
+
+
 @app.route("/")
 def index():
     return "This is index"
