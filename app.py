6ece2224bee6d5b564284dd1aa6055d5685e469b
rajkonkret <rajkonkret660@gmail.com>
2023-12-13 12:58:45 +0100

    1 - add exchange_results method and html with jinja

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,7 +10,18 @@ def index():
 
 @app.route('/exchange', methods=['GET', 'POST'])
 def exchange():
-    return render_template('exchange.html')
+    if request.method == 'GET':
+        return render_template('exchange.html')
+    else:
+        currency = 'EUR'
+        if 'currency' in request.form:
+            currency = request.form['currency']
+
+        amount = 100
+        if 'amount' in request.form:
+            amount = request.form['amount']
+
+        return render_template('exchange_results.html', currency=currency, amount=amount)
 
 
 if __name__ == '__main__':
